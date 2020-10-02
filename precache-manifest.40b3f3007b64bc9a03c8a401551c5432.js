ac394be80d3691b2b318cbcf94a380bf80371b57
Lucas <60299952+luc4slori@users.noreply.github.com>
2020-10-02 15:05:23 -0300

    Updates

---
+++ precache-manifest.40b3f3007b64bc9a03c8a401551c5432.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "156d7fa3c19d0b7a3d62b388492c52f7",
+    "url": "/searchApp/index.html"
+  },
+  {
+    "revision": "0f3db84a94386d7e82a0",
+    "url": "/searchApp/static/css/main.833eb3ff.chunk.css"
+  },
+  {
+    "revision": "cc1f3a5aae4ad765619b",
+    "url": "/searchApp/static/js/2.63ff56c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/searchApp/static/js/2.63ff56c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f3db84a94386d7e82a0",
+    "url": "/searchApp/static/js/main.12b6a8b7.chunk.js"
+  },
+  {
+    "revision": "21795249032fbee1f0e7",
+    "url": "/searchApp/static/js/runtime-main.0b97d6b4.js"
+  }
+]);
